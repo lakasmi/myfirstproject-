31aa1277adab774f927a3027456409ac9546a7fa
lakasmi <136238259+lakasmi@users.noreply.github.com>
2023-06-16 12:21:38 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,26 @@
+provider.tf
+terraform {
+
+  required_version = ">=0.12"
+  required_providers {
+
+    azurerm = {
+
+      source = "hashicorp/azurerm"
+
+      version = "~>2.0"
+
+    }
+
+  }
+
+  backend "azurerm" {
+
+  }
+}
+
+provider "azurerm" {
+  skip_provider_registration = "true"
+  features {}
+
+}
